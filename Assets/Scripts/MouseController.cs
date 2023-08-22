9175ec80cc60e2916fb25665ec0ef170649aa351
wirdschon100 <133209682+wirdschon100@users.noreply.github.com>
2023-08-22 09:29:29 +0200

    mouseMovementWorking

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public Vector2 turn;
+    public float multiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        multiplier = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * multiplier;
+        turn.y += Input.GetAxis("Mouse Y") * multiplier;
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+
+    public void changeMultiplier(float pMultiplier)
+    {
+        multiplier = pMultiplier;
+    }
+}
