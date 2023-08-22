8ff489a6196540eea3eaa400070451409b5b53a1
wirdschon100 <133209682+wirdschon100@users.noreply.github.com>
2023-08-22 12:30:39 +0200

    Sliding-start von kopie von "Advanced Sliding in 9 Minutes - Unity Tutorial"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -31,4 +31,3 @@ public class Movement : MonoBehaviour
        }
     }
 }
-*/
