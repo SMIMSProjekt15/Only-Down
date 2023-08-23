19a92ab567f3f6d6d87a38fa2612ff00569eb331
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2023-08-23 11:55:07 +0200

    Revert "Undo merge branch 'Sliding' into dev"
    This reverts commit e20c5aa1639c5d982f796b5bfe5fb0d28141cb32.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,6 @@ public class Movement : MonoBehaviour
     public float jumpForce = 5;
     public Rigidbody body;
     private int dontInfinetJump = 2;
-    public Sliding characterSlide;
-    public KeyCode SlideButton= KeyCode.LeftControl;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +19,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
        body.velocity = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal")*speedValue, body.velocity.y, Input.GetAxis("Vertical")*speedValue));
-        // Check for slide input
-        if (Input.GetKeyDown(SlideButton))
-        {
-            characterSlide.wantToSlide = true;
-        }
-
-        if (Input.GetKeyUp(SlideButton))
-        {
-            characterSlide.wantToSlide = false;
-        }
        if(Input.GetButtonDown("Jump")&& dontInfinetJump>0)
        {
             body.velocity = new Vector3(body.velocity.x, body.velocity.y + jumpForce, body.velocity.z);
