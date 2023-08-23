5246de51b7f1a08b9341e403765e092ef4b5dd78
Jaris <jaris.lamschick@outlook.de>
2023-08-23 17:05:38 +0200

    Main Menu

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -12,6 +12,7 @@ public class MouseController : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        ChangeSensitivity(SettingsMenu.sensitivity);
     }
 
     // Update is called once per frame
