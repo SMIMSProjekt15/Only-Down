d1afed5acfc59b7f8ffd3a2d10962ce7bc3dfd44
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2023-08-23 09:24:42 +0200

    Fixed World Collision and Player Movement

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    public Transform cam;
+    public Transform player;
     public Vector2 turn;
-    public float multiplier = 1;
+    public float sensitivityX, sensitivityY = 2;
     // Start is called before the first frame update
     void Start()
     {
-        multiplier = 1;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        turn.x += Input.GetAxis("Mouse X") * multiplier;
-        turn.y += Input.GetAxis("Mouse Y") * multiplier;
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        turn.x += Input.GetAxis("Mouse X") * sensitivityX;
+        turn.y = Mathf.Clamp(Input.GetAxis("Mouse Y") * sensitivityY + turn.y, -90, 90);
+        player.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        cam.transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+    
     }
 
-    public void changeMultiplier(float pMultiplier)
+    public void SetSensitivity(float pSensitivityX = 2, float pSensitivityY = 2)
     {
-        multiplier = pMultiplier;
+        sensitivityX = pSensitivityX;
+        sensitivityY = pSensitivityY;
     }
 }
