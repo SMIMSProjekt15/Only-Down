23ada627594f16cc2ab68672d39e8bf480adc65e
wirdschon100 <133209682+wirdschon100@users.noreply.github.com>
2023-08-23 16:45:03 +0200

    sliding und slope movement fertig

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -6,6 +6,7 @@ public class MouseController : MonoBehaviour
 {
     public Transform cam;
     public Transform player;
+    public Transform orientation;
     public Vector2 turn;
     public float sensitivityX, sensitivityY = 2;
     // Start is called before the first frame update
@@ -21,6 +22,7 @@ public class MouseController : MonoBehaviour
         turn.y = Mathf.Clamp(Input.GetAxis("Mouse Y") * sensitivityY + turn.y, -90, 90);
         player.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
         cam.transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+        orientation.rotation = Quaternion.Euler(0, turn.x, 0);
     
     }
 
