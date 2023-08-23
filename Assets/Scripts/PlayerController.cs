62dff7e8f15beb2307659e1e6d5ba471bd369f84
Jaris <jaris.lamschick@outlook.de>
2023-08-23 09:29:27 +0200

    Revert "Double Jump Problem"
    This reverts commit 05f5fc63c06b2ddf1314059079099426bfcc28d7.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,6 @@ public class Movement : MonoBehaviour
     public float jumpForce;
     public Rigidbody body;
     private int dontInfinetJump = 2;
-    private bool resetJumps = false;
 
     // Start is called before the first frame update
     void Start()
@@ -26,14 +25,9 @@ public class Movement : MonoBehaviour
             body.velocity = new Vector3(body.velocity.x, body.velocity.y + jumpForce, body.velocity.z);
             dontInfinetJump = dontInfinetJump -1;
        }
-       if (body.velocity.y<0)
-       {
-            resetJumps = true;
-       }
-       if (body.velocity.y == 0 && resetJumps == true)
+       if (body.velocity.y == 0)
        {
         dontInfinetJump = 2;
-        resetJumps = false;
        }
     }
 }
