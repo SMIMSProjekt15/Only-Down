93917d55ad5f89ea67a6cd75458c6bea20de2b9e
wirdschon100 <133209682+wirdschon100@users.noreply.github.com>
2023-08-24 16:06:16 +0200

    zwischenStand

---
+++ Assets/Scripts/Score/RowUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RowUI : MonoBehaviour
+{
+    public TextMeshPro rank;
+    public TextMeshPro name;
+    public TextMeshPro score;
+}
--- Assets/WinTrigger.cs
+++ Assets/WinTrigger.cs
@@ -6,7 +6,7 @@ public class WinTrigger : MonoBehaviour
 {
     public GameObject youWinText;
     // Start is called before the first frame update
-    void Start()
+    void Start()//setWinning
     {
         youWinText.SetActive(false);
     }
