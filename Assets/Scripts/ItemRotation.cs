a9fc60ae35541082d89362172c5524704066c44e
Jaris <jaris.lamschick@outlook.de>
2023-08-24 13:12:43 +0200

    Sachen

--- Assets/Scripts/ItemRotation.cs
+++ Assets/Scripts/ItemRotation.cs
@@ -15,7 +15,7 @@ public class ItemRotation : MonoBehaviour
     void Update()
     {
         this.gameObject.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-        this.gameObject.transform.localPosition = new Vector3(0, Mathf.Sin(Time.time) * 0.1f + 0.2f, 0);
+        this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x, this.gameObject.transform.localPosition.y + Mathf.Sin(Time.time) * 0.003f, this.gameObject.transform.localPosition.z);
     }
     
 }
